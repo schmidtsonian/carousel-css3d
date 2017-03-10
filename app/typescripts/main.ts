833c929529404b4059d18cace8da66a3ab8ecbcd
Esteban <eramirez@grupow.com>
2017-03-10 13:45:38 -0600

    refactor(scaffold)

--- app/typescripts/main.ts
+++ app/typescripts/main.ts
@@ -1,29 +1,31 @@
 /// <reference path="../definitions/jquery/jquery.d.ts" />
 
+
+//TODO: REFACTOR!!!!!!!!
 $(document).ready( function() {
     
     $('#js-bt__left').on('click', function() {
 
-        if($('#js-slider').hasClass('left') || $('#js-slider').hasClass('right')){
+        if($('#js-slider').hasClass('is-left') || $('#js-slider').hasClass('is-right')){
 
             $('#js-slider')
-                .removeClass("right")
-                .removeClass("left");
+                .removeClass("is-right")
+                .removeClass("is-left");
         } else {
 
-            $('#js-slider').addClass("left");
+            $('#js-slider').addClass("is-left");
         }
     });
     $('#js-bt__right').on('click', function() {
 
-        if($('#js-slider').hasClass('left') || $('#js-slider').hasClass('right')){
+        if($('#js-slider').hasClass('is-left') || $('#js-slider').hasClass('is-right')){
 
             $('#js-slider')
-                .removeClass("right")
-                .removeClass("left");
+                .removeClass("is-right")
+                .removeClass("is-left");
         } else {
 
-            $('#js-slider').addClass("right");
+            $('#js-slider').addClass("is-right");
         }
     });
 } );
